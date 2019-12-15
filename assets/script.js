81657874c7301fc260783520deab6aafe0d7eaf6
AJ <lastbreath.jones@gmail.com>
2019-12-14 19:59:31 -0700

    first commit

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+$("#port1").hover(function(){
+    $("#hidden1").css("display", "block")
+}, function(){
+    $("#hidden1").css("display", "none")
+})
+$("#port2").hover(function(){
+    $("#hidden2").css("display", "block")
+}, function(){
+    $("#hidden2").css("display", "none")
+})
+$("#port3").hover(function(){
+    $("#hidden3").css("display", "block")
+}, function(){
+    $("#hidden3").css("display", "none")
+})
